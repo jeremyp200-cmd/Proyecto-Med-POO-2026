b21b8fce1ec28f0b98848b1ebf7b7f803a186bcf
enymancia-cmd <enymancia@gmail.com>
2026-04-12 11:03:46 -0600

    Inventario de medicamentos

---
+++ Control-Med/Controllers/MedicamentosController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using ProyectoAPI.Models;
+using ProyectoAPI.Data;
+
+namespace ProyectoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicamentosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MedicamentosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Medicamentos);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Medicamento nuevo)
+        {
+            _context.Medicamentos.Add(nuevo);
+            _context.SaveChanges(); 
+            return Ok("Medicamento agregado");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var m = _context.Medicamentos.FirstOrDefault(x => x.Id == id);
+
+            if (m == null)
+                return NotFound("No encontrado");
+
+            _context.Medicamentos.Remove(m);
+            return Ok("Eliminado");
+        }
+    }
+}
